f19219f49ed861c30f734edffb5dfa79935a345b
Note07 <58530945+Note07@users.noreply.github.com>
2020-04-16 21:12:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace zad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //szukanie najwiekszej liczby z wpisanych
+
+            Console.Write("Wpisz liczby odzielając je przecinkami: ");
+            var a = Console.ReadLine();
+
+            var liczba = a.Split(','); 
+
+            var naj = Convert.ToInt32(liczba[0]);
+
+            foreach (var dane in liczba)
+            {
+                var cyfra = Convert.ToInt32(dane);
+                if (cyfra > naj)
+                    naj = cyfra;
+            }
+
+            Console.WriteLine("Największą z wpisanych jest: " + naj);
+        }
+    }
+}
